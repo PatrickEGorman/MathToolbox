e16c61dab6553ee60728aee612630ad3fc982972
PatrickEGorman <patrickgorman@localhost.localdomain>
2019-11-21 11:09:26 -0500

    initial commit

---
+++ src/main/java/MathToolbox/Editor/EditorController.java
@@ -0,0 +1,17 @@
+package MathToolbox.Editor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EditorController {
+
+    @GetMapping("/editor")
+    public String editor(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "editor/editor";
+    }
+
+}
